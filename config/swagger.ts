9b781ca640df6a005923314e0a2f81c4f45590fb
syarifhilmi <dhonisy@gmail.com>
2021-05-30 22:13:44 +0700

    swagger url test4

--- config/swagger.ts
+++ config/swagger.ts
@@ -3,8 +3,8 @@ import { SwaggerConfig } from '@ioc:Adonis/Addons/Swagger'
 export default {
 	uiEnabled: true, //disable or enable swaggerUi route
 	uiUrl: 'docs', // url path to swaggerUI
-	specEnabled: true, //disable or enable swagger.json route
-	specUrl: 'https://mainbersama.herokuapp.com/v1/swagger.json',
+	specEnabled: false, //disable or enable swagger.json route
+	specUrl: '/swagger.json',
 
 	middleware: [], // middlewares array, for protect your swagger docs and spec endpoints
 
