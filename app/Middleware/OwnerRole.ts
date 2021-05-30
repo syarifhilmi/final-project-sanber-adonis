da6714610f3abcd828805e1aa6bcd106c6420801
syarifhilmi <dhonisy@gmail.com>
2021-05-30 16:20:30 +0700

    make it better2

--- app/Middleware/OwnerRole.ts
+++ app/Middleware/OwnerRole.ts
@@ -1,7 +1,7 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class OwnerRole {
-  public async handle({ auth, response, request }: HttpContextContract, next: () => Promise<void>) {
+  public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
     let role = auth.user?.role
     if (role == 'owner') {
       await next()
